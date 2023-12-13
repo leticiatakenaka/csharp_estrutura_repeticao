da06052dba0712be5f287a625e8d67d1f14e1702
Leticia Santos Takenaka <leticiatakenaka12@gmail.com>
2023-12-13 08:47:15 -0300

    Exercício 1: Acesso com senha correta

---
+++ Exercicio_01/Program.cs
@@ -0,0 +1,12 @@
+﻿//Escreva um programa que repita a leitura de uma senha até que ela seja válida. Para cada leitura de senha 
+// incorreta informada, escrever a mensagem "Senha Invalida". Quando a senha for informada corretamente deve ser 
+// impressa a mensagem "Acesso Permitido" e o algoritmo encerrado. Considere que a senha correta é o valor 2002
+string senha;
+
+do
+{
+    Console.Write("Digite a senha: ");
+    senha = Console.ReadLine();
+
+    Console.WriteLine(senha != "2002" ? "Senha Invalida" : "Acesso Permitido");
+} while (senha != "2002");
